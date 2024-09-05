a5e2095ab2621823e6e8c2bbb39304df19163c9a
chell4 <arsenijalfeev@gmail.com>
2024-09-05 16:25:36 +0300

    Add box for boxing json

---
+++ boxes/jsonboxed/jsonboxed.go
@@ -0,0 +1,24 @@
+package jsonboxed
+
+import "encoding/json"
+
+type JsonBox[T any] struct {
+	value T
+}
+
+func (b *JsonBox[T]) Get() (T, error) {
+	return b.value, nil
+}
+
+func (b *JsonBox[T]) Put(t T) error {
+	b.value = t
+	return nil
+}
+
+func (b *JsonBox[T]) Marshal() ([]byte, error) {
+	return json.Marshal(b.value)
+}
+
+func (b *JsonBox[T]) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, &b.value)
+}
