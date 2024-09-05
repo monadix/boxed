33dceb014bf1a8c622325cfb0155b168133f6460
chell4 <arsenijalfeev@gmail.com>
2024-09-05 19:38:35 +0300

    Fix jsonboxed methods declarations

--- boxes/jsonboxed/jsonboxed.go
+++ boxes/jsonboxed/jsonboxed.go
@@ -3,26 +3,26 @@ package jsonboxed
 import "encoding/json"
 
 type JsonBox[T any] struct {
-	value T
+	value *T
 }
 
-func (b *JsonBox[T]) Get() (T, error) {
-	return b.value, nil
+func (b JsonBox[T]) Get() (T, error) {
+	return *b.value, nil
 }
 
-func (b *JsonBox[T]) Put(t T) error {
-	b.value = t
+func (b JsonBox[T]) Put(t T) error {
+	*b.value = t
 	return nil
 }
 
-func (b *JsonBox[T]) Marshal() ([]byte, error) {
+func (b JsonBox[T]) Marshal() ([]byte, error) {
 	return json.Marshal(b.value)
 }
 
-func (b *JsonBox[T]) Unmarshal(data []byte) error {
+func (b JsonBox[T]) Unmarshal(data []byte) error {
 	return json.Unmarshal(data, &b.value)
 }
 
 func New[T any](val T) JsonBox[T] {
-	return JsonBox[T]{value: val}
+	return JsonBox[T]{value: &val}
 }
